887e22b2a59c9e11364bcf571918fe935bf433ba
Irian <icortesdev@proton.me>
2024-04-03 13:06:01 +0200

    inventory

--- src/pages/inventory.tsx
+++ src/pages/inventory.tsx
@@ -1,21 +1,29 @@
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
-import React from 'react'
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import React from 'react';
 
-const inventory = () => {
-
-
-    
-
+const Inventory: React.FC = () => {
     return (
         <>
             <Navbar />
-            <div className='Galeria'>
-                
+            <div className="galery flex justify-center">
+                <div className="p-4 mt-2 mb-6  border-b-2 border-t-2  border-black text-center">
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-2 ">
+                        {[...Array(12)].map((_, index) => (
+                            <div key={index}>
+                                <img
+                                    className="w-36 h-auto max-w-full rounded-lg"
+                                    src={`https://flowbite.s3.amazonaws.com/docs/gallery/square/image-${index + 1}.jpg`}
+                                    alt={`Image ${index + 1}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
             <Footer />
         </>
-    )
-}
+    );
+};
 
-export default inventory
+export default Inventory;
--- src/pages/linking.tsx
+++ src/pages/linking.tsx
@@ -30,6 +30,12 @@ const linking = () => {
                     <div className='tokens'>
                         <button className='AlphaToken' type='submit'>Alpha Token </button>
                     </div>
+                    <div className='cadena'>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="border border-black ml-4">
+                            <path d="M12 20.1C10.29 20.1 8.9 18.71 8.9 17V13H7L7 17C7 19.76 9.24 22 12 22C14.76 22 17 19.76 17 17V13H15.1V17C15.1 18.71 13.71 20.1 12 20.1ZM13 16V8H11V16H13ZM7 7V11H8.9V7C8.9 5.29 10.29 3.9 12 3.9C13.71 3.9 15.1 5.29 15.1 7L15.1 11H17V7C17 4.24 14.76 2 12 2C9.24 2 7 4.24 7 7Z" fill="black"></path>
+                        </svg>
+                    </div>
+
                     <button className='LinkingToken'>
                         V-LINKING TOKEN
                     </button>
@@ -38,7 +44,7 @@ const linking = () => {
                     <button
                     >{/* Agrega la lógica para link to nft */}LINK NFT</button>
                 </div>
-            </div>
+            </div >
 
             <Footer />
         </>
