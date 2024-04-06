c5e75c65c38ebfc04a430b00f13b71967268e4cd
Irian <icortesdev@proton.me>
2024-04-06 14:40:44 +0200

    navbar & image1

--- src/components/Ilustracion.tsx
+++ src/components/Ilustracion.tsx
@@ -6,7 +6,7 @@ const Ilustracion = () => {
         <>
             <div className="ilustracion-container">
                 <div className="ilustracion hidden sm:block">
-                    <Image className='ilust' src="/ilustración.png" alt="viñeta1" width="1500" height="1080" />
+                    <Image className='ilust' src="/Ilustracion.png" alt="viñeta1" width="1500" height="1080" />
                 </div>
             </div>
         </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,35 +38,36 @@ const Navbar = () => {
                     <Link href="/">
                         <Image src="/Isotipo_Negro.svg" alt="logo" width={30} height={30} priority />
                     </Link>
-                    <button
-                        data-collapse-toggle="navbar-default"
-                        type="button"
-                        className="inline-flex items-center ml-40 p-2 w-10 h-10 justify-center text-sm bg-gray-400 text-black border border-black mr-1 rounded-lg md:hidden hover:black focus:outline-none"
-                        aria-controls="navbar-default"
-                        aria-expanded={menuVisible ? 'true' : 'false'}
-                        onClick={toggleMenu}
-                        style={{ borderRightWidth: '4px' }}
-                    >
-                        <span className="sr-only">Abrir menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-
-
-                    </button>
-                    <div className={`w-start md:w-${menuVisible ? 'full' : '80'} ${menuVisible ? '' : 'hidden md:block'}`} id="navbar-default">
-                        <strong> |</strong> <Link className="linking-link hover:text-gray-500" id="wallet-connect" href="/linking" passHref>
-                            <strong> LINKING</strong>
-                        </Link>  <strong> |</strong>
-                        <Link className="inventory-link hover:text-gray-500" id="wallet-connect" href="/inventory" passHref>
-                            <strong>  INVENTORY </strong>
-                        </Link> <strong> |</strong>
-                    </div>
-                    <div className="btn-nav">
-                        <Link className="wallet-button text-button wp-button" id="wallet-connect" href="/" passHref>
-                            WALLET
-                        </Link></div>
                 </div>
+                <button
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    className="inline-flex items-center ml-40 p-2 w-10 h-10 justify-center text-sm bg-gray-400 text-black border border-black mr-1 rounded-lg md:hidden hover:black focus:outline-none"
+                    aria-controls="navbar-default"
+                    aria-expanded={menuVisible ? 'true' : 'false'}
+                    onClick={toggleMenu}
+                    style={{ borderRightWidth: '4px' }}
+                >
+                    <span className="sr-only">Open menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+
+
+                </button>
+                <div className={`w-start md:w-${menuVisible ? 'full' : '80'} ${menuVisible ? '' : 'hidden md:block'}`} id="navbar-default">
+                    <strong> |</strong> <Link className="linking-link hover:text-gray-500" id="wallet-connect" href="/linking" passHref>
+                        <strong> LINKING</strong>
+                    </Link>  <strong> |</strong>
+                    <Link className="inventory-link hover:text-gray-500" id="wallet-connect" href="/inventory" passHref>
+                        <strong>  INVENTORY </strong>
+                    </Link> <strong> |</strong>
+                </div>
+                <div className="btn-nav">
+                    <Link className="wallet-button text-button wp-button" id="wallet-connect" href="/" passHref>
+                        WALLET
+                    </Link></div>
+
 
             </section >
         </>
